494d03aa7d0611a98f327675287027243ea4a722
options3224@gmail.com <options3224@gmail.com>
2022-11-21 16:46:55 +0900

    메인 UI에서 검색 관련 컴포넌트 추가

--- src/FrameSearch.java
+++ src/FrameSearch.java
@@ -5,37 +5,69 @@ import javax.swing.table.*;
 
 public class FrameSearch extends JFrame {
 	private static final long serialVersionUID = 1L;
-
-	JTable table = new JTable(); // 테이블 객체(테이블 껍데기?)
-	String header[] = { "문화재이름", "시대", "소재지/출토지", "상태" }; // 테이블 헤더
-	String contents[][] = {
-			{ "서울 숭례문", "조선시대", "서울 중구", "보존" },
-			{ "서울 원각사지 십층석탑", "조선시대", "서울 종로구", "보존" },
-			{ "서울 흥인지문", "조선시대", "서울 종로구", "보존" },
-			{ "옛 보신각 동종", "조선시대", "서울 용산구", "보존" } }; // 테이블 내용
-
-	// 테이블 모델(테이블 알맹이를 모아놓은 것?)
-	DefaultTableModel tableModel = new DefaultTableModel(contents, header) {
-		private static final long serialVersionUID = 1L;
-		public boolean isCellEditable(int row, int column) {
-			return false;
-		}
-	};
+	
+	private JPanel contentPane;
+	private JTable table = new JTable();
+	private JTextField textField;
 
 	// 생성자
 	public FrameSearch() {
+		initFrame();
+		initPanels();
+		initSearchComponents();
+		initTable();
+	}
+	
+	// Frame 초기화
+	public void initFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		// 해당 프레임을 종료하면 프로그램 전체 종료
 		setTitle("문화재 관리 프로그램");
-		setSize(600, 300);
-
-		add(new JScrollPane(table));
-		table.addMouseListener(new MyMouseAdapter());
-		table.setModel(tableModel);
-		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); // 하나만 선택 가능
-
+		setSize(600, 350);
 		setVisible(true);
 	}
-
+	
+	// Panel 초기화
+	public void initPanels() {
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+	}
+	
+	// 검색 관련 컴포넌트 초기화
+	public void initSearchComponents() {
+		textField = new JTextField();
+		textField.setBounds(12, 23, 459, 27);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnSearch = new JButton("검색");
+		btnSearch.setBounds(483, 23, 91, 27);
+		contentPane.add(btnSearch);
+	}
+	
+	// 검색 결과 테이블 초기화
+	public void initTable() {
+		String header[] = { "문화재이름", "시대", "소재지/출토지", "상태" }; // 테이블 헤더
+		String contents[][] = {
+				{ "서울 숭례문", "조선시대", "서울 중구", "보존" },
+				{ "서울 원각사지 십층석탑", "조선시대", "서울 종로구", "보존" },
+				{ "서울 흥인지문", "조선시대", "서울 종로구", "보존" },
+				{ "옛 보신각 동종", "조선시대", "서울 용산구", "보존" } }; // 테이블 내용
+		
+		table.addMouseListener(new MyMouseAdapter());
+		contentPane.setLayout(null);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); // 하나만 선택 가능
+		table.setModel(new DefaultTableModel(contents, header) {
+			private static final long serialVersionUID = 1L;
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		});
+		
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setBounds(12, 71, 562, 232);
+		contentPane.add(scrollPane);
+	}
+	
 	// 내부 클래스: 상세 페이지 생성자의 매개변수로 문화재명(기본키)을 전달함
 	class MyMouseAdapter extends MouseAdapter {
 		public void mouseClicked(MouseEvent e) {
